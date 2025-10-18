fbd9363f5fe920f0bf858ec26816ace6334b5f31
dev-gupta-dev <devgithub2002@gmail.com>
2025-10-18 12:03:01 +0530

    Load Iris dataset and create DataFrame

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
-# Load built-in Iris dataset
+
 iris = load_iris()
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 df["species"] = iris.target
